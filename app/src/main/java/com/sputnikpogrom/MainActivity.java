1b428d132f72f4c36ccd7097670feecf34f3bebc
VEINHORN <mcklas@mail.ru>
2014-10-13 22:45:25 +0300

    Overrided methods for main menu.

--- app/src/main/java/com/sputnikpogrom/MainActivity.java
+++ app/src/main/java/com/sputnikpogrom/MainActivity.java
@@ -3,6 +3,9 @@ package com.sputnikpogrom;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -39,5 +42,21 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.mainmenu_settings:
+                break;
+        }
+        return false;
     }
 }
